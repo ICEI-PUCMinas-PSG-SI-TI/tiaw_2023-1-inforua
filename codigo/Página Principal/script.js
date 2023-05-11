65a50e237bfd4e1a108fb8f6749e66fa1c52b5e3
hayanness <hayanne.santos@sga.pucminas.br>
2023-05-11 11:15:57 -0300

    Comentarios sendo enviados ao feed

--- codigo/Página Principal/script.js
+++ codigo/Página Principal/script.js
@@ -1,18 +1,16 @@
-document.querySelector(".hamburger").addEventListener("click", () =>
-    document.querySelector(".container").classList.toggle("show-menu")
-);
 const inputName = document.getElementById('inputName');
-const textComment = document.getElementById('inputText');
+const inputText = document.getElementById('inputText');
 const form = document.getElementById('formulario');
 const commentPost = document.getElementById('commentPost');
+const commentBody = document.getElementById('commentBody');
 
 form.addEventListener("submit",(event)=>{
     event.preventDefault();
 
     let p= document.createElement('p');
     p.classList = 'p-2 d-flex text-wap flex-wap';
-    p.innerHTML = textComment.value;
+    p.innerHTML = `<strong>${inputName.value}</strong> ${inputText.value}`;
     commentPost.appendChild(p);
-    textComment.value = '';
-
+    inputText.value = '';
+    
 });
